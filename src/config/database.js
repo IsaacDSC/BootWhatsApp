3b069435dddf9002804ec5d31274a4686b1d02bc
Matheus Mendes Rezende <matheusmendesr7@gmail.com>
2020-09-08 21:04:55 -0300

    make it better

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,7 @@
 module.exports = {
-    dialect: 'postgres',
-    host: 'ec2-54-91-178-234.compute-1.amazonaws.com',
-    username: 'monbswjbljqmnw',
-    password: '400ebdbb5ad5f37ca94352e894f5444814582ee7afb5827a4610f009281126c2',
-    database: 'd93s26ordomgs9',
-    port: 5432
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'dev',
+    password: 'secret',
+    database: 'bootwhatsapp'
 }
