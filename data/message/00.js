802842085b8b8728c0544f65f7235f563b1682f4
IsaacDSC <isaac8.silva@hotmail.com>
2020-09-11 09:11:28 -0300

    removendo arquivos desnecesários

--- data/message/00.js
+++ data/message/00.js
@@ -2,29 +2,28 @@ require('module-alias/register')
     //const cardapio = require('@data/cardapio/inicio')
 const banco = require('@data/user/user')
 const Cardapio = require('@models/Menu')
-const User = require('@models/Users')
 const escolha = require("../escolha");
 const setStage = require('../../src/helpers/setStage')
 
 let key = 0
 
 async function execute(user, msg, contato) {
-    let menu ='🔢 Digite o *número* da categoria:\n\n ```Digite apenas 1 número.```\n\n';
+    let menu = '🔢 Digite o *número* da categoria:\n\n ```Digite apenas 1 número.```\n\n';
 
     //Cardapio Obtido Do Banco de Dados só Obtem as classes
     const cardapio = await Cardapio.findAll({
         attributes: ['class'],
         group: ['class']
     })
-    
+
 
     //passa cada primeira letra para maiuscula
     cardapio.forEach((e, index) => {
         escolha.db.push({ 'id': index + 1, 'class': e.dataValues.class })
         return menu += `*[ ${index+1} ]* ${e.dataValues.class.toUpperCase()} \n`
     })
-    
-    menu+='\nDica:\nse quer *'+escolha.db[0].class.toUpperCase()+'* envie o número *1*.\n\n───────────────'
+
+    menu += '\nDica:\nse quer *' + escolha.db[0].class.toUpperCase() + '* envie o número *1*.\n\n───────────────'
 
     if (key === 1) {
         //Nome da pessoa Digitado = contato
@@ -34,7 +33,7 @@ async function execute(user, msg, contato) {
         //console.log('\n\n' + user + '\n\n')
         // passando user para estagio 01
         setStage.envStageDb(user, 0)
-        
+
 
         return [
             `Olá, *${contato}* sou uma assistente virtual, irei apresentar o carpádio, para fazer o pedido basta enviar o codigo do produto`,
@@ -49,13 +48,13 @@ async function execute(user, msg, contato) {
         //console.log('\n\n' + user + '\n\n')
         // passando user para estagio 01
         setStage.envStageDb(user, 1)
-       
+
         return [
             `Olá, *${contato}* sou uma assistente virtual, irei apresentar o carpádio, para fazer o pedido basta enviar o codigo do produto`,
             menu,
         ];
     } else {
-         setStage.envStageDb(user, 0)
+        setStage.envStageDb(user, 0)
 
         banco.db[user].stage = 0;
         key = 1
--- data/message/01.js
+++ data/message/01.js
@@ -2,9 +2,7 @@ require('module-alias/register')
 const cardapio = require('@data/cardapio/promocoes')
 const escolha = require("../escolha");
 const Menu = require('@models/Menu')
-const Requests = require('@models/Requests')
 const banco = require('@data/user/user')
-const User = require('@models/Users')
 const setStage = require('../../src/helpers/setStage')
 
 let key = 0;
@@ -28,43 +26,45 @@ async function execute(user, msg) {
         banco.db[user].stage = 2;
         return ["Estamos fechando seu pedido, ok?"];
     }
-    
-    const quantidadedeEscolhas= escolha.db.filter(e=>{return e.id}).length
+
+    const quantidadedeEscolhas = escolha.db.filter(e => { return e.id }).length
     console.log(Number(msg))
-    if (msg>quantidadedeEscolhas || !Number(msg)) {
+    if (msg > quantidadedeEscolhas || !Number(msg)) {
         return [
-            "Você *precisa* escolher um número da categoria."];
-    }else{
-    
-    //Adiciona o item ao carrinho 
-    
-    if (key === 1) {
-        banco.db[user].itens.push(cardapio.menu[msg]);
-        const itemEscolhido= escolha.db.filter(e=>{return e.index ==msg}) 
+            "Você *precisa* escolher um número da categoria."
+        ];
+    } else {
 
-    //Coloca o Item escolhido do usuario ao banco de dados 
-        
-        return [`Item(${itemEscolhido[0].name}) adiconado com sucesso `,
-            frase,
-        ]
-        
-    }else{
-         // Numero Digitado pega a class
-        const classe= escolha.db.filter(e=>{return e.id ==msg}) 
-        clas= classe[0].class
-        const itensMenu = await Menu.findAll({where:{class:clas}})
-        let menu = '🔢 Digite o *número* do produto:\n\n ```Digite apenas 1 número.```\n\n'
-        menu += ` ${clas.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); })} \n\n`;
-        key = 1
+        //Adiciona o item ao carrinho 
 
-        itensMenu.forEach((e,index)=>{
-            escolha.db.push({'index':index+1,'name':e.dataValues.name, 'price':e.dataValues.value})
+        if (key === 1) {
+            banco.db[user].itens.push(cardapio.menu[msg]);
+            const itemEscolhido = escolha.db.filter(e => { return e.index == msg })
 
-           return menu += `${index+1} - ${e.dataValues.name}        R$ ${e.dataValues.value} \n`;
-        })
-        return [menu, frase];
-}}
- 
+            //Coloca o Item escolhido do usuario ao banco de dados 
+
+            return [`Item(${itemEscolhido[0].name}) adiconado com sucesso `,
+                frase,
+            ]
+
+        } else {
+            // Numero Digitado pega a class
+            const classe = escolha.db.filter(e => { return e.id == msg })
+            clas = classe[0].class
+            const itensMenu = await Menu.findAll({ where: { class: clas } })
+            let menu = '🔢 Digite o *número* do produto:\n\n ```Digite apenas 1 número.```\n\n'
+            menu += ` ${clas.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); })} \n\n`;
+            key = 1
+
+            itensMenu.forEach((e, index) => {
+                escolha.db.push({ 'index': index + 1, 'name': e.dataValues.name, 'price': e.dataValues.value })
+
+                return menu += `${index+1} - ${e.dataValues.name}        R$ ${e.dataValues.value} \n`;
+            })
+            return [menu, frase];
+        }
+    }
+
 
 }
 
