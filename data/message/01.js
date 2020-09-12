4223c209317c12864c32540abe88c2baacd305c9
Matheus Mendes Rezende <matheusmendesr7@gmail.com>
2020-09-11 23:28:55 -0300

    ok

--- data/message/01.js
+++ data/message/01.js
@@ -6,21 +6,22 @@ const User = require('@models/Users')
 const Requests = require('@models/Requests')
 const banco = require('@data/user/user')
 const setStage = require('../../src/helpers/setStage')
-
+const a = require('../../src/helpers/getMenu')
 let key = 0;
 
 async function execute(user, msg) {
 
     const frase = "```Digite # para finalizar * para cancelar & para voltar ao cardápio```"
+    let menu
+    await a.getMenu().then((res) => menu = res.toString())
 
-    if (msg.toUpperCase() === "V") {
-       
-        return ["v"];
+    if (msg.toUpperCase() === "V" || msg.toUpperCase() === "E" && key==3) {
+        key = 0
+        return [menu];
     }
 
     if (msg === "*") {
         setStage.envStageDb(user, 0)
-
         key = 0
         banco.db[user].stage = 0;
         banco.db[user] = ""
@@ -40,33 +41,47 @@ async function execute(user, msg) {
     })
 
     //quantidade de classes 
-    if (msg > quantidadedeEscolhas.length && key == 0 || !Number(msg) ) {
+    if (msg > quantidadedeEscolhas.length && key == 0 || !Number(msg)&& key==0) {
         return [
             "Você *precisa* escolher um número da categoria."
         ];
-    }
-
-    //Adiciona o item ao carrinho
-    if (key === 1) {
-        const itemEscolhido = escolha.db.filter(e => { return e.index == msg })
+    } 
+     //Adiciona o item ao carrinho
+    //Cadastra o Pedido No banco de Dados
+    //msg = ao item escolhido
+    if(key===2){
+        const itemEscolhido =await escolha.db.filter(e => { return e.index == msg })
         const UserId = await User.findAll({ where: { telephone: user }, attributes: ['id'] })
         const MenuNameId = await Menu.findAll({ where: { name: itemEscolhido[0].name }, attributes: ['id'] })
-        //Só adiciona um item
+        
         Requests.create({
             MenuNameId: MenuNameId[0].dataValues.id,
             UserId: UserId[0].dataValues.id,
-            quantity: 1,
+            quantity: msg,
             status: 0
         }).then(() => console.log('Produto Cadastrado Para O Usuario'))
-        .catch((err)=>console.log(err))
+            .catch((err) => console.log(err))
 
+        key=1
+        return['🔢  Quantos produtos *'+itemEscolhido[0].name+'* iguais a este você quer pedir?\n\n *Digite um número para gravar este produto.*']
+    }
+    
+    if(key===1 && !Number(msg) || msg>=100){
+    
+        return['🔢  Quantidade muito alta.\nLimite máximo por pedido 100 unidades.']
+    }
+
+   
+    //msg = quantidade de itens
+    if (key === 1) {
+        key=3
+        
+        console.log(`Quantidade(${msg}) adiconado com sucesso `)
         banco.db[user].itens.push(cardapio.menu[msg]);
 
         //Coloca o Item escolhido do usuario ao banco de dados 
 
-        return [`Item(${itemEscolhido[0].name}) adiconado com sucesso `,
-            frase,
-        ]
+        return [`👏  Produto *gravado* no carrinho.` ,'Deseja escolher *outro* produto?\n\n───────────────\n\n*[ E ]* ESCOLHER OUTRO PRODUTO\n*[ M ]* ESCOLHER MAIS *AÇAÍ*\n\n*[ F ]* *PARA FECHAR O PEDIDO*']
 
     } else {
         // Numero Digitado pega a class
@@ -74,7 +89,7 @@ async function execute(user, msg) {
 
         const itensMenu = await Menu.findAll({ where: { class: classe } })
         let menu = '🔢 Digite o *número* do produto:\n\n ```Digite apenas 1 número.```\n\n'
-        key = 1
+        key = 2
 
         itensMenu.forEach((e, index) => {
             escolha.db.push({ 'index': index + 1, 'name': e.dataValues.name, 'price': e.dataValues.value })
