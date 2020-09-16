f1264219617d5a0a55ef22684ebba43e13d2bb49
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-09-16 19:35:05 -0300

    make it better

--- test/SQL.js
+++ test/SQL.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'db4free.net',
     port: '3306',
-    user: 'dev',
-    password: 'secret',
+    user: 'matheusmlol',
+    password: '12052000a',
     database: 'bootwhatsapp'
 })
+
+  /* 
+module.exports = {
+    dialect: 'mysql',
+    host: 'db4free.net',
+    username: 'matheusmlol',
+    password: '12052000a',
+    database: 'bootwhatsapp'
+}*/
 
 let sql = 'select users.name, users.telephone, menu_requests.delivery from users of menu_requests on users.id = menu_requests.MenuNameId;'
 connection.query(sql, (err, result) => {
