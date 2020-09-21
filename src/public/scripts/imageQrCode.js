68e620c15e868d0df28d76ea8d956b5d2d3ec569
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-09-21 13:53:28 -0300

    ok

--- src/public/scripts/imageQrCode.js
+++ src/public/scripts/imageQrCode.js
@@ -3,3 +3,16 @@ setInterval(()=>{
 
 },4000)
 
+document.addEventListener('DOMContentLoaded', function () {
+    const checkbox = document.querySelector('input[type="checkbox"]');
+    
+      checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+           jQuery('.modal').modal();
+        } else {
+      
+          console.log('Not checked');
+        }
+      });
+    });
+    
