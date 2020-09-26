c9be0114f75012cd57329e6804405bac393d3c5d
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-09-26 12:05:16 -0300

    ok

--- src/public/scripts/carregaCliente.js
+++ src/public/scripts/carregaCliente.js
@@ -1,7 +1,10 @@
 var select = document.getElementById('telephone')
+let selectCardapio = document.getElementById('selectClassCardapio')
+let btnPesquisar = document.getElementById('pesquisarClasses')
+let itensCardapio = document.getElementById('itensDoCardapio')
 
 select.addEventListener('change', e => {
-    var value = select.options[select.selectedIndex].value;
+    let value = select.options[select.selectedIndex].value;
 
     $.ajax({
         type: "POST",
@@ -17,5 +20,37 @@ select.addEventListener('change', e => {
         $('#addressClient').val(result.address);
         $('#idUsuarios').val(result.id)
     })
+
+})
+
+btnPesquisar.addEventListener('click', async(e) => {
+    let value = selectCardapio.options[selectCardapio.selectedIndex].value;
+    if(!value){
+        return
+    }
+   await $.ajax({
+        type: "POST",
+        url: 'http://localhost:3001/caixa/pesquisaCliente',
+        data: { telephone: value },
+        success: console.log('Pesquisado Com Sucesso')
+    })
+
+
+    let html =`
+    <div class="form-group col-6">
+    <label for="">${value.toUpperCase()}</label>
+    <select name="" id="" class="form-control">
+        <option value="">Selecione</option>
+        <option value=""></option>
+    </select>
+    </div>
+    <div class="form-group col-2">
+    <label for="">Qtd</label>
+    <input type="text" name="" id="" class="form-control">
+    </div>`
+
+ itensCardapio.insertAdjacentHTML('afterbegin', html);
+
+   console.log(value)
 
 })
