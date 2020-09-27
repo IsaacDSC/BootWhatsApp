a9681f752bf1ebe1a43503d3819cc7758abb2d66
IsaacDSC <isaac8.silva@hotmail.com>
2020-09-27 20:07:19 -0300

    dando primeira arrumada no vizualizar clients

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -4,38 +4,23 @@ const router = express.Router()
 const { auth } = require('@helpers/auth')
 const db = require('@database/configSQL')
 
-router.get('/clients/:pag', auth, (req, res) => {
-    // if (req.params.pag == +1) {
-    //     let id = Number(req.params.pag) + 1
-    //     console.log(id)
-    //     let sql = `SELECT * FROM users ORDER BY id=${id} DESC LIMIT 1;`
-    //     db.connection.query(sql, (err, result) => {
-    //         res.render('clients/clients', { clients: result })
-    //     })
-    // }
-    // if (req.params.pag == -1) {
-    //     console.log(id)
-    //     let id = Number(req.params.pag) - 1
-    //     let sql = `SELECT * FROM users ORDER BY id=${id} DESC LIMIT 1;`
-    //     db.connection.query(sql, (err, result) => {
-    //         res.render('clients/clients', { clients: result })
-    //     })
-    // } else {
-    //     let id = Number(req.params.pag)
-    //     let sql = `SELECT * FROM users ORDER BY id=${id} DESC LIMIT 1;`
-    //     db.connection.query(sql, (err, result) => {
-    //         let id = result[0].id
-    //         console.log(result[0].id)
-    //         res.render('clients/clients', { clients: result, id: id })
-    //     })
-    // }
-    let id = Number(req.params.pag)
-    let sql = `SELECT * FROM users ORDER BY id=${req.params.pag} DESC LIMIT 10;`
+router.get('/clients/:pag', (req, res) => {
+    let parametro = req.params.pag
+    let sql = `SELECT * FROM users ORDER BY id=${parametro} DESC LIMIT 10;`
+    let sql_totalClients = `SELECT COUNT(id) FROM users;`
     db.connection.query(sql, (err, result) => {
-        let id = result[0].id
-        console.log(result[0].id)
-        res.render('clients/clients', { clients: result, id: id })
-    })
+            db.connection.query(sql_totalClients, (err, totalClientes) => {
+                totalClientes.forEach(element => {
+                    console.log(element)
+                    if (element == 4) {
+                        console.log('ok')
+                    }
+                });
+                res.render('clients/clients', { clients: result, totalClientes: totalClientes })
+            })
+        })
+        //res.send(parametro)
+    console.log(parametro)
 
 })
 
