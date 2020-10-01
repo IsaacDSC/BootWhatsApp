6d4311baed683f00cc1e21746ff86f34c52aa18d
IsaacDSC <isaac8.silva@hotmail.com>
2020-10-01 20:27:54 -0300

    tentando imprimir

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -6,16 +6,24 @@ const path = require('path')
 const folder = path.resolve(__dirname + '', '../', 'public', 'images', 'avatar')
 const multer = require('multer')
 
+
 //config multer 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, folder)
         console.log(file)
+        const isAccepted = ['image/png', 'image/jpg', 'image/jpeg']
+        if (file.mimetype == isAccepted[0] || file.mimetype == isAccepted[1] || file.mimetype == isAccepted[2]) {
+            cb(null, folder)
+        } else {
+            return cb(false)
+        }
+        //cb(null, folder)
+
     },
     filename: function(req, file, cb) {
         //function para contar arquivos
         fs.readdir(folder, (err, paths) => {
-            //def nomes do arquivos
+            //def nomes do arquivos            
             cb(null, 'profile.png')
         })
     }
@@ -23,11 +31,9 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage,
     fileFilter: (req, res, cb) => {
+        // console.log(mimeType)
         cb(null, true)
-            // if (file.mimetype == "image/jpg") {
-            // } else {
-            //     cb(null, false)
-            // }
+
     }
 })
 
