b2de8fa653a152c25787249d583bca7bda44879b
Matheus Mendes Rezende <60859810+matheusm821@users.noreply.github.com>
2020-10-22 15:38:37 -0300

    ok

--- src/config/bot.js
+++ src/config/bot.js
@@ -50,10 +50,9 @@ ev.on('qr.**', async qrcode => {
 });
 
 const launchConfig = {
-    qrTimeout: 60000 * 10,
+    qrTimeout: false,
     autoRefresh: true,
-     chromiumArgs: ['--no-sandbox'],
-
+    chromiumArgs: ['--no-sandbox'],
 };
 
 async function client() {
