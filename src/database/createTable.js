ccc33bf090f33fd3a1a26d7d34a607d22789f654
IsaacDSC <isaac8.silva@hotmail.com>
2021-01-13 17:34:07 -0300

    upgrade

--- src/database/createTable.js
+++ src/database/createTable.js
@@ -6,18 +6,43 @@ const Menu = require('@models/Menu')
 const User = require('@models/Users')
 const Admin = require('@models/Admin')
 const Relacionamento = require('@models/Relacionamento')
+const ClassMenu = require('@models/classMenu')
+const Message = require('@models/Messages')
+const RegistersUsers = require('@models/RegistersUsers')
+const Requests = require('@models/Requests')
+const RoteDelivery = require('@models/RoteDelivery')
 
-createTable()
+let MODELS = [Requests, Menu, User, Admin, Relacionamento, ClassMenu, Message, RegistersUsers, RoteDelivery]
 
-async function createTable() {
-    //Menu.sync({ force: true })
-    //  User.sync({ force: true })
-    // Requests.sync({ force: true })
-    //Menu.belongsToMany(Menu, { through: 'relacionamentos', foreignKey: 'cardapioId', as: 'menus' })
-    //User.belongsToMany(User, { through: 'relacionamentos', foreignKey: 'UserId', as: 'users' })
-    //Requests.belongsToMany(Requests, { through: 'relacionamentos', foreignKey: 'PedidosId', as: 'requests' })
-    Relacionamento.sync({ froce: true })
-        Admin.sync({ force: true })
-        await db.sync({ force: true })
-        //  Resquests.sync({ force: true })
+(async function() {
+    MODELS.forEach((element, index) => {
+        element.sync({ force: true })
+            .then(() => { console.log(index, element) })
+            .catch((err) => { console.log(index, element, err) })
+    })
+})()
+
+function relationship() {
+    Menu.belongsToMany(Menu, { through: 'relacionamentos', foreignKey: 'cardapioId', as: 'menus' })
+    User.belongsToMany(User, { through: 'relacionamentos', foreignKey: 'UserId', as: 'users' })
+    Requests.belongsToMany(Requests, { through: 'relacionamentos', foreignKey: 'PedidosId', as: 'requests' })
 }
+
+setTimeout(() => {
+    relationship()
+}, 120000)
+
+// createTable()
+
+// async function createTable() {
+//     //Menu.sync({ force: true })
+//     //  User.sync({ force: true })
+//     // Requests.sync({ force: true })
+//     //Menu.belongsToMany(Menu, { through: 'relacionamentos', foreignKey: 'cardapioId', as: 'menus' })
+//     //User.belongsToMany(User, { through: 'relacionamentos', foreignKey: 'UserId', as: 'users' })
+//     //Requests.belongsToMany(Requests, { through: 'relacionamentos', foreignKey: 'PedidosId', as: 'requests' })
+//     Relacionamento.sync({ froce: true })
+//     Admin.sync({ force: true })
+//     await db.sync({ force: true })
+//         //  Resquests.sync({ force: true })
+// }
